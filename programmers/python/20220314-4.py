975d5271d469f3c5b26033b0836e6ffaebdded97
lagunerio <lagunerio@gmail.com>
2022-03-14 22:41:04 +0900

    프로그래머스 - 코딩테스트 연습 > 2020 KAKAO BLIND RECRUITMENT > 문자열 압축

---
+++ programmers/python/20220314-4.py
@@ -0,0 +1,31 @@
+# 코딩테스트 연습 > 2019 KAKAO BLIND RECRUITMENT > 오픈채팅방
+from collections import defaultdict
+import json
+
+def solution(records):
+	answer = []
+	users = defaultdict()
+	log = []
+
+	for record in records:
+		record_type = record.split(" ")[0]
+
+		if record_type == "Leave":
+			uid = record.split(" ")[1]
+			log.append([uid, record_type])
+		elif record_type == "Enter":
+			uid, nickname = record.split(" ")[1:]
+			users[uid] = nickname
+			log.append([uid, record_type])
+		elif record_type == "Change":
+			uid, nickname = record.split(" ")[1:]
+			users[uid] = nickname
+
+	message = {
+		"Enter": "님이 들어왔습니다.",
+		"Leave": "님이 나갔습니다."
+	}
+	for raw in log:
+		answer.append(f"{users[raw[0]]}{message[raw[1]]}")
+
+	return answer
